e4805b58f92bb73046b2cfb173b5f87c63b4931e
jmigsdj <miggydj@gmail.com>
2019-03-28 02:36:04 +0800

    Homepage added Refuel Module Added  (+) crud  (+) list

--- app/controllers/refuels_controller.rb
+++ app/controllers/refuels_controller.rb
@@ -1,2 +1,56 @@
 class RefuelsController < ApplicationController
+  def index
+    @refuels = Refuel.all
+  end
+  
+  def show
+    @refuel = Refuel.find(params[:id])
+  end
+  
+  def new
+    @refuel = Refuel.new
+  end
+  
+  def edit
+    @refuel = Refuel.find(params[:id])
+  end
+  
+  def create
+    @refuel = Refuel.new(refuel_params)
+    
+    if @refuel.save
+      redirect_to @refuel
+    else
+      render 'new' 
+    end
+  end
+
+  def update
+    @refuel = Refuel.find(params[:id])
+
+    if @refuel.update(refuel_params)
+      redirect_to @refuel
+    else
+      render 'edit'      
+    end
+  end
+
+  def destroy
+    @refuel = Refuel.find(params[:id])
+    @refuel.destroy
+
+    redirect_to refuels_path
+  end
+
+  private
+    def refuel_params
+      params.require(:refuel).permit(
+        :date, 
+        :driver, 
+        :vehicle, 
+        :odometer_reading, 
+        :refuel_location, 
+        :fuel_litters)
+    end
+
 end
--- app/models/refuel.rb
+++ app/models/refuel.rb
@@ -1,2 +1,8 @@
 class Refuel < ApplicationRecord
+  validates :date, presence: true
+  validates :driver, presence: true
+  validates :vehicle, presence: true
+  validates :odometer_reading, presence: true
+  validates :refuel_location, presence: true
+  validates :fuel_litters, presence: true
 end
